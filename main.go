be2b52e847db78a9694f4791a451a329d5f49ea4
Brian Waweru <brianwaweru627@gmail.com>
2025-10-01 10:25:13 +0300

    feat: loaded env variables, configs, connected to the db, added logger and cors in the middleware and registered api routes. Note that when you run this application you'll get this error since there are no migrations creating the mkwanja db: server error: FATAL: database "mkwanja" does not exist (SQLSTATE 3D000)

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"mkwanja-payment-service/internal/configs"
+	"mkwanja-payment-service/internal/routes"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	// Load environment variables
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	// Load config
+	cfg := configs.LoadConfig()
+
+	// Connect to database
+	db, err := gorm.Open(postgres.Open(cfg.DatabaseURL()), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	// Create Fiber app
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Dex!")
-	})
+	// Middleware
+	app.Use(logger.New())
+	app.Use(cors.New())
 
-	app.Listen(":3000")
+	// Register routes
+	routes.RegisterApiRoutes(app, db, cfg)
+
+	// Start server
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Starting server on port %s", port)
+	log.Fatal(app.Listen(":" + port))
 }
